c93ec724c7c92c5ae7e2644b191946a5e2460cbb
nickrepaci <nickrepaci@gmail.com>
2020-10-14 00:23:42 -0400

    updated class

--- Chatter/Controllers/RegisterViewController.swift
+++ Chatter/Controllers/RegisterViewController.swift
@@ -5,9 +5,12 @@
 //  Created by Nicholas Repaci on 10/13/20.
 //
 
-import Foundation
 import UIKit
 
 class RegisterViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
     
 }
