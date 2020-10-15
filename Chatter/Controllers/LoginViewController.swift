58e392c52ff884d58f3f1e5a48a4656300676990
nickrepaci <nickrepaci@gmail.com>
2020-10-15 19:52:44 -0400

    Added some button outlets

--- Chatter/Controllers/LoginViewController.swift
+++ Chatter/Controllers/LoginViewController.swift
@@ -9,8 +9,18 @@ import Foundation
 import UIKit
 
 class LoginViewController: UIViewController {
+    
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    
+    @IBAction func loginButtonPressed(_ sender: UIButton) {
+    }
+    
     
 }
--- Chatter/Controllers/RegisterViewController.swift
+++ Chatter/Controllers/RegisterViewController.swift
@@ -9,8 +9,15 @@ import UIKit
 
 class RegisterViewController: UIViewController {
     
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var registerButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
+    @IBAction func registerButton(_ sender: UIButton) {
+    }
 }
